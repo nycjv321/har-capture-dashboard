3bfeddbc6cf83cd59c30f140fad234299431c7a6
Javier Velasquez <nycjv321@gmail.com>
2015-09-13 03:13:39 -0400

    added css minification and scss compiling to gulp workflow. added d3 library. started basic bootstrap powered layout.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var uglify = require("gulp-uglify");
 var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
+
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -11,7 +15,8 @@ gulp.task('js', function() {
       [
         'bower_components/bootstrap/dist/js/bootstrap.min.js',
         'bower_components/jquery/dist/jquery.min.js',
-        'bower_components/jquery/dist/jquery.min.map'
+        'bower_components/jquery/dist/jquery.min.map',
+        'bower_components/d3/d3.min.js'
       ]
   ).pipe(
       gulp.dest('public/javascripts')
@@ -29,6 +34,23 @@ gulp.task('css', function() {
   ).pipe(
       gulp.dest('public/stylesheets')
   );
+
+  gulp.src(
+      [
+      'resources/stylesheets/*.scss'
+      ]
+  ).pipe(
+      sass()
+  //).pipe(
+  //    sourcemaps.init()
+  ).pipe(
+      minifyCss()
+  //).pipe(
+  //    sourcemaps.write()
+  ).pipe(
+        gulp.dest('public/stylesheets')
+  );
+
 });
 
 gulp.task('fonts', function() {
